165ca700c92a46a0d2a470f0e01015f558be9a0c
Julia <offo4ka94@gmail.com>
2020-09-28 15:35:26 +0300

    properties

--- src/test/java/tests/BaseTest.java
+++ src/test/java/tests/BaseTest.java
@@ -24,12 +24,26 @@ public class BaseTest {
     public final static String ZIP = "12345";
 
     @BeforeMethod
+//    public void createDriver(ITestContext context) {
+//
+//        try {
+//            driver = new ChromeDriver(CapabilitiesGenerator.getChromeOptions());
+//        } catch (SessionNotCreatedException ex) {
+//            Assert.fail("Браузер не был открыт. Проверьте, что используется корректная версия драйвера");
+//            log.fatal(ex.getLocalizedMessage());
+//        }
+//
+//        String variable = "driver";
+//        System.out.println("Setting driver into context with variable name " + variable);
+//        context.setAttribute(variable, driver);
+//    }
     public void setUp(){
         //System.setProperty("webdriver.opera.driver", "src/test/resources/operadriver.exe");
         //driver = new OperaDriver();
+        System.setProperty("webdriver.chrome.driver", "src/test/resources/webdrivers/linux/chromedriver");
         driver = new ChromeDriver(CapabilitiesGenerator.getChromeOptions());
         driver.manage().window().maximize();
-        driver.manage().timeouts().implicitlyWait(100, TimeUnit.SECONDS);
+        driver.manage().timeouts().implicitlyWait(20, TimeUnit.SECONDS);
         loginPage = new LoginPage(driver);
         productsPage = new ProductsPage(driver);
         cartPage = new CartPage(driver);

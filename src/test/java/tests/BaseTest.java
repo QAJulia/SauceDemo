979faf9479412ece84a4f5b2b361cc59e32a9c63
Julia <offo4ka94@gmail.com>
2020-09-28 18:19:32 +0300

    basetest

--- src/test/java/tests/BaseTest.java
+++ src/test/java/tests/BaseTest.java
@@ -1,6 +1,9 @@
 package tests;
+import org.openqa.selenium.SessionNotCreatedException;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.Assert;
+import org.testng.ITestContext;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Listeners;
@@ -23,6 +26,18 @@ public class BaseTest {
     public final static String ZIP = "12345";
 
     @BeforeMethod
+    public void createDriver(ITestContext context) {
+        try {
+            driver = new ChromeDriver(CapabilitiesGenerator.getChromeOptions());
+        } catch (SessionNotCreatedException ex) {
+            Assert.fail("Браузер не был открыт. Проверьте, что используется корректная версия драйвера");
+
+        }
+        String variable = "driver";
+        System.out.println("Setting driver into context with variable name " + variable);
+        context.setAttribute(variable, driver);
+    }
+
     public void setUp(){
         //System.setProperty("webdriver.opera.driver", "src/test/resources/operadriver.exe");
         //driver = new OperaDriver();

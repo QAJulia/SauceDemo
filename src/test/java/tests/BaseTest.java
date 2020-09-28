3eb35c738e3ba7ab0663cb9c7d5d2b515be19ff6
Julia <offo4ka94@gmail.com>
2020-09-28 15:51:18 +0300

    cg

--- src/test/java/tests/BaseTest.java
+++ src/test/java/tests/BaseTest.java
@@ -1,7 +1,6 @@
 package tests;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
-import org.openqa.selenium.opera.OperaDriver;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Listeners;
@@ -24,23 +23,9 @@ public class BaseTest {
     public final static String ZIP = "12345";
 
     @BeforeMethod
-//    public void createDriver(ITestContext context) {
-//
-//        try {
-//            driver = new ChromeDriver(CapabilitiesGenerator.getChromeOptions());
-//        } catch (SessionNotCreatedException ex) {
-//            Assert.fail("Браузер не был открыт. Проверьте, что используется корректная версия драйвера");
-//            log.fatal(ex.getLocalizedMessage());
-//        }
-//
-//        String variable = "driver";
-//        System.out.println("Setting driver into context with variable name " + variable);
-//        context.setAttribute(variable, driver);
-//    }
     public void setUp(){
         //System.setProperty("webdriver.opera.driver", "src/test/resources/operadriver.exe");
         //driver = new OperaDriver();
-        System.setProperty("webdriver.chrome.driver", "src/test/resources/webdrivers/linux/chromedriver");
         driver = new ChromeDriver(CapabilitiesGenerator.getChromeOptions());
         driver.manage().window().maximize();
         driver.manage().timeouts().implicitlyWait(20, TimeUnit.SECONDS);

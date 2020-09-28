ba1bf4d4c1431fb329e1ab477c1a7b84ad8a3991
Julia <offo4ka94@gmail.com>
2020-09-28 14:54:41 +0300

    allure

--- src/test/java/tests/BaseTest.java
+++ src/test/java/tests/BaseTest.java
@@ -1,13 +1,16 @@
 package tests;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.opera.OperaDriver;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Listeners;
 import pages.*;
 import utils.CapabilitiesGenerator;
 
 import java.util.concurrent.TimeUnit;
 
+@Listeners(TestListener.class)
 public class BaseTest {
     WebDriver driver;
     LoginPage loginPage;
@@ -22,8 +25,9 @@ public class BaseTest {
 
     @BeforeMethod
     public void setUp(){
-        //System.setProperty("webdriver.opera.driver", "src/test/resources/operadriver.exe");
-        driver = new ChromeDriver(CapabilitiesGenerator.getChromeOptions());
+        System.setProperty("webdriver.opera.driver", "src/test/resources/operadriver.exe");
+        driver = new OperaDriver();
+        //driver = new ChromeDriver(CapabilitiesGenerator.getChromeOptions());
         driver.manage().window().maximize();
         driver.manage().timeouts().implicitlyWait(100, TimeUnit.SECONDS);
         loginPage = new LoginPage(driver);
